dab8622e067363f8bb4a30c5d7ef8576df67753b
Ishani Ghosh <ishani28ghosh@rediffmail.com>
2024-08-29 16:54:43 -0400

    fixed issues with formatting/linting/sorting/type checking

--- cybulde/version_data.py
+++ cybulde/version_data.py
@@ -1,9 +1,9 @@
-from pathlib import Path
+
 
 from cybulde.config_schemas.config_schema import Config
 from cybulde.utils.config_utils import get_config
-from cybulde.utils.data_utils import initialize_dvc
-from cybulde.utils.utils import get_logger
+from cybulde.utils.data_utils import initialize_dvc, commit_to_dvc, initialize_dvc_storage
+
 
 
 @get_config(config_path="../configs", config_name="config")
@@ -11,7 +11,10 @@ def version_data(config: Config) -> None:
     print(config)
     
     initialize_dvc()
+
     initialize_dvc_storage(config.dvc_remote_name, config.dvc_remote_url)
+
+    make_new_data_version(config.dvc_raw_data_folder, config.dvc_remote_name)
 
 if __name__ == "__main__":
     version_data()  # type: ignore
