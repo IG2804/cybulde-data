862bdd78b75800199ba6398999289b992cdefd30
Ishani Ghosh <ishani28ghosh@rediffmail.com>
2024-08-28 22:25:02 -0400

    added functionality to initialize dvc storage

--- cybulde/version_data.py
+++ cybulde/version_data.py
@@ -8,9 +8,10 @@ from cybulde.utils.utils import get_logger
 
 @get_config(config_path="../configs", config_name="config")
 def version_data(config: Config) -> None:
-
+    print(config)
+    
     initialize_dvc()
-
+    initialize_dvc_storage(config.dvc_remote_name, config.dvc_remote_url)
 
 if __name__ == "__main__":
     version_data()  # type: ignore
